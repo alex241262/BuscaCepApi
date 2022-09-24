c897e0bfbfdd973ca9889c665b7c04a0172e4a7b
alex241262 <alexandreferreira168@gmail.com>
2022-09-24 18:31:51 -0400

    Projeto finalizado

--- BuscaCepApi/MainWindow.xaml.cs
+++ BuscaCepApi/MainWindow.xaml.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using BuscaCepApi.Entities;
+using BuscaCepApi.Services;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -18,11 +22,44 @@ namespace BuscaCepApi
     /// <summary>
     /// Interação lógica para MainWindow.xam
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void NotifyPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+        private BuscaCepApic buscaCepapic;
+        public BuscaCepApic BuscaCepApic
+        {
+            get { return buscaCepapic; }
+            set { buscaCepapic = value; NotifyPropertyChanged(); }
+        }
+        public async void ConsultaBuscaCepApic()
+        {
+            
+            string Cep = txtbox1.Text;
+            try
+            {
+                var service = new BuscaCepApiService();
+                var buscaCepapic = await service.GetBuscaCepApicRandom(Cep);
+
+                if (buscaCepapic == null) return;
+                BuscaCepApic = buscaCepapic;
+            }
+            catch (Exception exx)
+            {
+                MessageBox.Show(exx.Message);
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ConsultaBuscaCepApic();
         }
     }
 }
